8fe1f48e1a9c068185b40b65a67b0a2a9d580995
Kartofle <adamrose9058@gmail.com>
2018-01-08 22:28:24 -0800

    added post, update, delete to api

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var mongoose = require('mongoose');
 var config = require('./config');
 var setupController = require('./controllers/setupController');
+var apiController = require('./controllers/apiController');
 
 var port = process.env.PORT || 3000;
 
@@ -10,8 +11,9 @@ app.use('/assets', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
-mongoose.connect(config.getDBConnectionString());
+mongoose.connect(config.getDBConnectionString(), { useMongoClient: true });
 setupController(app);
+apiController(app);
 
 app.listen(port);
 
